0a43733d7210adb4b279ccc5d50fd91eb6646165
bhanuprakash056 <r19cs056@cit.reva.edu.in>
2023-04-25 19:06:49 +0530

    Merge pull request #2 from tjcdeveloper/patch-1
    Update structs.go

--- src/lectures/exercise/arrays/arrays.go
+++ src/lectures/exercise/arrays/arrays.go
@@ -18,6 +18,29 @@ package main
 
 import "fmt"
 
-func main() {
+type Product struct {
+	name  string
+	price int
+}
 
+func printStat(list [4]Product) {
+	var cost, totalItems int
+	for i := 0; i < len(list); i++ {
+		item := list[i]
+		cost += item.price
+		if item.name != "" {
+			totalItems += 1
+		}
+	}
+	fmt.Println("cost  : ", cost)
+	fmt.Println("total items", totalItems)
+	fmt.Println("last item ", list[totalItems-1].name)
+}
+func main() {
+	shoppinglist := [4]Product{
+		{"banana", 12},
+		{"apple", 23},
+		{"bread", 22},
+	}
+	printStat(shoppinglist)
 }
